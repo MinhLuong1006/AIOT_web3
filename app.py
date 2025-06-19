1ecabe0b313577b43e134e3f9fbc7fe882f41c23
Minh_Luong <minhluong10062006@gmail.com>
2025-06-19 18:24:02 +0700

    app

--- app.py
+++ app.py
@@ -51,6 +51,7 @@ def validate_email(email):
     pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     return re.match(pattern, email) is not None
 
+
 def validate_password(password):
     """Validate password strength"""
     if len(password) < 8:
